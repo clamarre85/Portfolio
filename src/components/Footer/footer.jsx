f247e024508c4facabde2e8dc11bb84dcb1eb991
clamarre85 <clamarre85@outlook.com>
2024-10-28 20:09:09 -0400

    modified

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './footer.css'
+import footer_logo from '../../assets/footer_logo.svg'
+import user_icon from '../../assets/user_icon.svg'
 
 const Footer = () => {
   return (
-    <div>
-    
+    <div className='footer'>
+        <div className="footer-top">
+            <div className="footer-top-left">
+                <img src={footer_logo} alt="" />
+                <p> I am a frontend developer from Canada with over 10 years experience in customer service excellence.</p>
+            </div>
+            <div className="footer-top-right">
+                <div className="footer-email-input">
+                  <img src="user_icon" alt=" />"
+                  <input type="email" placeholder='Enter your email' />                
+            </div>
+            <div className=" footer-subscribe">Subscribe</div>    
+        </div>          
+      </div>
+      <hr/>
     </div>
   )
 }
